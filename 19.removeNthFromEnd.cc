df6ff297fd89aae5d50ba13b52851f8e3cdf8aa6
mahaoyang <mahaoyang@appledeMacBook-Pro.local>
2023-05-09 13:21:47 +0800

    虚拟头节点真好用喵

---
+++ 19.removeNthFromEnd.cc
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // if(head==nullptr||head->next==nullptr)return nullptr;
+
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode* p1=dummy;
+        ListNode* p2=dummy;
+
+        for(int i=0;i<n;i++)
+        {
+            p1=p1->next;
+        }
+
+        while(p1->next!=nullptr)
+        {
+            p1=p1->next;
+            p2=p2->next;
+        }
+        // ListNode* tmp=p2->next;
+        p2->next=p2->next->next;
+        // free(tmp);
+
+        return dummy->next;
+    }
+};
