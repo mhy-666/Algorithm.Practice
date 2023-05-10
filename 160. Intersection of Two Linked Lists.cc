0f09574ddbc4777094f97030bfa2f3d3417a7a6e
mhy-666 <1950994@tongji.edu.cn>
2023-05-10 23:02:22 +0800

    concat linked list

--- 160. Intersection of Two Linked Lists.cc
+++ 160. Intersection of Two Linked Lists.cc
@@ -1,4 +1,4 @@
-
+#include<cstdlib>
  // Definition for singly-linked list.
   struct ListNode {
       int val;
@@ -9,18 +9,26 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* p1=headA;
-        ListNode* p2=headB;
-
+        ListNode *p1 = headA;   
+        ListNode *p2 = headB;
+    
+        
         while(p1!=p2)
         {
-            if(p1!=nullptr)
+            if(p1!=nullptr&&p2!=nullptr)
             {
-            p1=p1->next;
-            p2=p2->next;
+              p1 = p1->next;
+              p2 = p2->next;
+              
+            } 
+            else if(p1==NULL&&p2!=NULL) {
+              p1 = headB;
+            } else if (p2 == NULL&&p1!=NULL) {
+              p2 = headA;
+            } else {
+              break;
             }
-            
-
         }
+        return p1;
     }
 };
